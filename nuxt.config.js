c01894d2d0a2b3e42fd376be1ff7370d632ac0bb
Ulil Albab Surya Negara <surya.negara@gontor.ac.id>
2021-11-29 20:05:02 +0100

    add seo tags, add google analytics, move credential to env variables

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,15 +4,15 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'parkirmana-landingpage',
+    title: 'ParkirMana — Modern Parking Solution',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { hid: 'description', name: 'description', content: 'Single platform for connected mobility. Make it easy for you to find and pay for parking and book the best spot for you before you go.' },
+      { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -35,12 +35,19 @@ export default {
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
     '@nuxtjs/tailwindcss',
-    '@nuxtjs/google-fonts'
+    '@nuxtjs/google-fonts',
+    '@nuxtjs/google-analytics',
   ],
 
   googleFonts: {
     families: {
       Mulish: ['200','300','400','500','600','700','800']
+    }
+  },
+
+  googleAnalytics: {
+    options: {
+      id: process.env.GOOGLE_ANALYTICS_ID,
     }
   },
 
@@ -50,15 +57,15 @@ export default {
       '@nuxtjs/firebase',
       {
         config: {
-          apiKey: 'AIzaSyCMW5iwjaxTiEcrckqmKXPjF8w9MViPwfI',
-          authDomain: 'parkirmana.firebaseapp.com',
-          projectId: 'parkirmana',
-          storageBucket: 'parkirmana.appspot.com',
-          messagingSenderId: '574994447276',
-          appId: '1:574994447276:web:37a678126c8dc049cd1809',
+          apiKey: process.env.FIREBASE_API_KEY,
+          authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+          messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+          appId: process.env.FIREBASE_APP_ID,
         },
         services: {
-          auth: true, // Just as example. Can be any other service.
+          auth: true,
           firestore: true
         }
       }
